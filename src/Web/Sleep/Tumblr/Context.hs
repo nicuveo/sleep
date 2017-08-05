95833362045494063f16c0391571ff565e4388c0
Nicuvëo <crucuny@gmail.com>
2017-08-05 12:48:17 +0100

    Improved Context.
    * added helpers for basic use case
    * overloaded FromJSON for ()
    * fixed HasHTTP* interfaces

--- src/Web/Sleep/Tumblr/Context.hs
+++ src/Web/Sleep/Tumblr/Context.hs
@@ -4,6 +4,7 @@
 {-# LANGUAGE KindSignatures        #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeFamilies          #-}
+{-# LANGUAGE UndecidableInstances  #-}
 
 
 
@@ -12,6 +13,9 @@
 module Web.Sleep.Tumblr.Context (
        HasHTTPGet(..),
        HasHTTPPost(..),
+       anonymously,
+       withKey,
+       withAuth,
        call,
        callT,
        callE,
@@ -27,45 +31,103 @@ import           Control.Monad.Reader
 import           Data.Aeson.Types
 import           Data.ByteString.Lazy
 import           Data.Proxy
+import           Network.HTTP.Conduit
 import           Network.URL
 
 import           Web.Sleep.Tumblr.Error
+import           Web.Sleep.Tumblr.Methods
 import           Web.Sleep.Tumblr.Query
 import           Web.Sleep.Tumblr.Response
 
 
 
--- exported types
+-- network interfaces
 
-class HasHTTPGet c where
-  httpGet  :: Monad m => c -> URL -> m ByteString
+class Monad m => HasHTTPGet c m where
+  httpGet  :: c -> URL -> m ByteString
 
-class HasHTTPPost c where
-  httpPost :: Monad m => c -> URL -> m ByteString
+class Monad m => HasHTTPPost c m where
+  httpPost :: c -> URL -> m ByteString
 
 
 
--- exported functions
+-- helper functions for simple usage
 
-call  :: (QueryResult q ~ r, HTTPMethod (QueryProtocol q) c, MonadReader c m, FromJSON r)
+anonymously :: MonadIO m => ReaderT NoContext m r -> m r
+anonymously = flip runReaderT NoContext
+
+withKey :: MonadIO m => APIKey -> ReaderT JustAPIKey m r -> m r
+withKey = flip runReaderT . JustAPIKey
+
+withAuth :: APIKey -> AuthToken -> ReaderT AuthAndKey m r -> m r
+withAuth k t = flip runReaderT $ AuthAndKey (k, t)
+
+
+
+-- actual network call and parsing
+
+call  :: (QueryResult q ~ r, HTTPMethod (QueryProtocol q) c m, MonadReader c m, FromJSON' r)
           => Query q -> m (Either Error r)
-callT :: (QueryResult q ~ r, HTTPMethod (QueryProtocol q) c, MonadReader c m, FromJSON r, MonadThrow m)
+callT :: (QueryResult q ~ r, HTTPMethod (QueryProtocol q) c m, MonadReader c m, FromJSON' r, MonadThrow m)
           => Query q -> m r
-callE :: (QueryResult q ~ r, HTTPMethod (QueryProtocol q) c, MonadReader c m, FromJSON r, MonadError Error m)
+callE :: (QueryResult q ~ r, HTTPMethod (QueryProtocol q) c m, MonadReader c m, FromJSON' r, MonadError Error m)
           => Query q -> m r
-call  q = fmap getResponse $ getRawData q =<< ask
-callT q = getResponseT   =<< getRawData q =<< ask
-callE q = getResponseE   =<< getRawData q =<< ask
+call  q = ask >>= getRawData q >>= decode
+callT q = ask >>= getRawData q >>= decodeT
+callE q = ask >>= getRawData q >>= decodeE
 
 
 
--- internal helpers
+-- internal simple contexts
 
-class HTTPMethod (p :: QProtocol) c where
+data    NoContext  = NoContext
+newtype JustAPIKey = JustAPIKey APIKey
+newtype AuthAndKey = AuthAndKey (APIKey, AuthToken)
+
+instance HasAPIKey    JustAPIKey where getAPIKey    (JustAPIKey k)      = k
+instance HasAPIKey    AuthAndKey where getAPIKey    (AuthAndKey (k, _)) = k
+instance HasAuthToken AuthAndKey where getAuthToken (AuthAndKey (_, t)) = t
+
+instance MonadIO m => HasHTTPGet NoContext m where
+  httpGet _ = simpleHttp . exportURL
+
+instance MonadIO m => HasHTTPGet JustAPIKey m where
+  httpGet _ = simpleHttp . exportURL
+
+instance MonadIO m => HasHTTPGet AuthAndKey m where
+  httpGet _ = simpleHttp . exportURL
+
+instance MonadIO m => HasHTTPPost AuthAndKey m where
+  httpPost _ = undefined
+
+
+
+-- internal network method abstraction
+
+class HTTPMethod (p :: QProtocol) c m where
   getRawData :: (QueryProtocol q ~ p, Monad m) => Query q -> c -> m ByteString
 
-instance (HasHTTPGet c) => HTTPMethod QGet c where
+instance (HasHTTPGet c m) => HTTPMethod QGet c m where
   getRawData q c = httpGet c $ toURL q
 
-instance (HasHTTPPost c) => HTTPMethod QPost c where
+instance (HasHTTPPost c m) => HTTPMethod QPost c m where
   getRawData q c = httpPost c $ toURL q
+
+
+
+-- overlapping getResponse for ()
+
+class FromJSON a => FromJSON' a where
+  decode  :: Monad m            => ByteString -> m (Either Error a)
+  decodeT :: MonadThrow m       => ByteString -> m a
+  decodeE :: MonadError Error m => ByteString -> m a
+
+instance {-# OVERLAPPABLE #-} FromJSON a => FromJSON' a where
+  decode  = return . getResponse
+  decodeT = getResponseT
+  decodeE = getResponseE
+
+instance {-# OVERLAPPING #-} FromJSON' () where
+  decode  _ = return $ Right ()
+  decodeT _ = return ()
+  decodeE _ = return ()
