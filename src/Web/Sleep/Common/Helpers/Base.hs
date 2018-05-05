7546553b3893c9d73cf6a1ff13cc53da6e7bf459
Nicuvëo <crucuny@gmail.com>
2018-05-05 21:49:13 +0100

    Added Helper common files.
    Those will probably be renamed as external, or moved to their own cabal file:
    they are simply wrappers or re-implementations or fixes of existing packages.
    
    Base fixes Control.Monad.Base by using type families and by supporting
    ResourceT.
    
    Request adds modifiers to Network.HTTP.Client, much like the
    http-client-request-modifiers package, but covers only a subset of cases.

---
+++ src/Web/Sleep/Common/Helpers/Base.hs
@@ -0,0 +1,65 @@
+{-# LANGUAGE DefaultSignatures     #-}
+{-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies          #-}
+
+
+
+
+-- module
+
+module Web.Sleep.Common.Helpers.Base where
+
+import           Control.Monad.Trans.Class
+import           Control.Monad.Trans.Cont
+import           Control.Monad.Trans.Except
+import           Control.Monad.Trans.Identity
+import           Control.Monad.Trans.List
+import           Control.Monad.Trans.Maybe
+import           Control.Monad.Trans.Reader
+import           Control.Monad.Trans.Resource
+import qualified Control.Monad.Trans.RWS.Lazy      as L
+import qualified Control.Monad.Trans.RWS.Strict    as S
+import qualified Control.Monad.Trans.State.Lazy    as L
+import qualified Control.Monad.Trans.State.Strict  as S
+import qualified Control.Monad.Trans.Writer.Lazy   as L
+import qualified Control.Monad.Trans.Writer.Strict as S
+import           Data.Functor.Identity
+-- import Control.Monad.Trans.Accum
+-- import Control.Monad.Trans.Select
+
+
+class Monad m => MonadBase m where
+  type Base m :: * -> *
+  liftBase :: Base m a -> m a
+  default liftBase :: (MonadTrans t, MonadBase b, m ~ t b, Base m ~ Base b) => Base m a -> m a
+  liftBase = liftDefault
+
+
+instance MonadBase IO         where { type Base IO         = IO      ; liftBase = id }
+instance MonadBase Maybe      where { type Base Maybe      = Maybe   ; liftBase = id }
+instance MonadBase (Either e) where { type Base (Either e) = Either e; liftBase = id }
+instance MonadBase ((->) r)   where { type Base ((->) r)   = ((->) r); liftBase = id }
+instance MonadBase Identity   where { type Base Identity   = Identity; liftBase = id }
+
+instance MonadBase m => MonadBase (IdentityT  m)  where type Base (IdentityT  m) = Base m
+instance MonadBase m => MonadBase (MaybeT     m)  where type Base (MaybeT     m) = Base m
+instance MonadBase m => MonadBase (ListT      m)  where type Base (ListT      m) = Base m
+instance MonadBase m => MonadBase (ReaderT  r m)  where type Base (ReaderT  r m) = Base m
+instance MonadBase m => MonadBase (L.StateT s m)  where type Base (L.StateT s m) = Base m
+instance MonadBase m => MonadBase (S.StateT s m)  where type Base (S.StateT s m) = Base m
+instance MonadBase m => MonadBase (ContT    r m)  where type Base (ContT    r m) = Base m
+instance MonadBase m => MonadBase (ExceptT  e m)  where type Base (ExceptT  e m) = Base m
+instance MonadBase m => MonadBase (ResourceT  m)  where type Base (ResourceT  m) = Base m
+-- instance MonadBase m => MonadBase (SelectT  r m)  where type Base (SelectT  r m) = Base m
+
+instance (Monoid w, MonadBase m) => MonadBase (L.WriterT w     m) where type Base (L.WriterT w     m) = Base m
+instance (Monoid w, MonadBase m) => MonadBase (S.WriterT w     m) where type Base (S.WriterT w     m) = Base m
+instance (Monoid w, MonadBase m) => MonadBase (L.RWST    r w s m) where type Base (L.RWST    r w s m) = Base m
+instance (Monoid w, MonadBase m) => MonadBase (S.RWST    r w s m) where type Base (S.RWST    r w s m) = Base m
+-- instance (Monoid w, MonadBase m) => MonadBase (AccumT    w     m) where type Base (AccumT    w     m) = Base m
+
+
+liftDefault :: (MonadTrans t, MonadBase b, m ~ t b, Base m ~ Base b) => Base m a -> m a
+liftDefault = lift . liftBase
---
+++ src/Web/Sleep/Common/Helpers/Request.hs
@@ -0,0 +1,39 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+
+
+-- module
+
+module Web.Sleep.Common.Helpers.Request (
+  appendParam,
+  appendParams,
+  appendHeader,
+  setBody,
+  ) where
+
+
+
+-- imports
+
+import qualified Data.ByteString           as B
+import           Data.List                 (foldl')
+import qualified Network.HTTP.Client       as N
+import qualified Network.HTTP.Types.Header as N
+
+
+
+-- request helpers
+
+appendParam :: (B.ByteString, B.ByteString) -> N.Request -> N.Request
+appendParam p req = req { N.queryString = append p $ N.queryString req }
+  where append (name, val) "" = B.concat [         name, "=", val]
+        append (name, val) qs = B.concat [qs, "&", name, "=", val]
+
+appendParams :: [(B.ByteString, B.ByteString)] -> N.Request -> N.Request
+appendParams = flip $ foldl' $ flip appendParam
+
+appendHeader :: N.HeaderName -> B.ByteString -> N.Request -> N.Request
+appendHeader hn hv req = req { N.requestHeaders = N.requestHeaders req ++ [ (hn, hv) ] }
+
+setBody :: B.ByteString -> N.Request -> N.Request
+setBody body req = req { N.requestBody = N.RequestBodyBS body }
