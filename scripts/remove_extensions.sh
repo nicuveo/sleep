d86d7c97f01133d1381f545b864271e4002c9e7c
Nicuvëo <crucuny@gmail.com>
2018-05-06 01:24:57 +0100

    Added autocleaning script.

---
+++ scripts/remove_extensions.sh
@@ -0,0 +1,50 @@
+#! /usr/bin/env bash
+
+set -e
+
+FILES=$(find src -type f -iname "*.hs")
+
+
+# assert we are at the repo root
+
+if [ ! -r "stack.yaml" ] ; then
+    echo "not in repo root, aborting"
+    exit 1
+fi
+
+
+# assert that the repo is clean
+
+if [ -n "$(git st --porcelain | egrep -v '^\?\?')" ] ; then
+    echo "repo not clean, aborting"
+    exit 1
+fi
+
+
+# checking out new branch
+
+git checkout -B autoclean
+stack clean
+
+
+# do the thing
+
+for f in $FILES ; do
+    exts=$(sed -n 's/{-# LANGUAGE \([^ ]*\) *#-}/\1/p' < $f)
+    if [ -n "$exts" ] ; then
+        echo "# $f"
+        for ext in $exts ; do
+            sed -i "/LANGUAGE $ext/d" $f
+            set +e
+            stack test --fast --ghc-options '-Werror' &> /dev/null
+            res=$?
+            set -e
+            if [ $res -eq 0 ] ; then
+                echo "  removing $ext from $f"
+                git commit $f -m "removed $ext from $f" &> /dev/null
+            else
+                git reset --hard &> /dev/null
+            fi
+        done
+    fi
+done
